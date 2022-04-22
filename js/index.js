df7b0c5815fedaf627287b33e1c436cda239a00e
Ed Izotov <ed.izotov@gmail.com>
2022-04-22 17:22:04 +0200

    aefraswe

--- js/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-	
-	var myButton = document.querySelector('#clickbutton')
-	var myTextbox = document.querySelector('#textbox')
-	
-	myButton.addEventListener('click', function() {
-		myTextbox.innerText = 'VASYA'
-	})	
-})
